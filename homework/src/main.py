42ea3d404c50c83f57edd58dd48c332d96f229d0
Camilo Sánchez <yosanchezm@unal.edu.co>
2025-12-06 12:03:37 -0500

    Fix: Import os module and set MLflow tracking URI dynamically

--- homework/src/main.py
+++ homework/src/main.py
@@ -1,4 +1,5 @@
 ## Se importa MLflow
+import os
 import uuid
 
 import mlflow  # type: ignore
@@ -16,6 +17,9 @@ RANDOM_STATE = 123456
 
 
 def main():
+
+    tracking_uri = os.path.join(os.getcwd(), "mlruns")
+    mlflow.set_tracking_uri(f"file://{tracking_uri}")
 
     args = parse_argument()
     model = select_model(args)
